b10a23a53d8bd555f932663f1a557711b4e47cfd
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2022-03-20 14:49:24 -0600

    Primero programa agregado

---
+++ Parcial_1/hijos.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argC, char *argV[]){
+    
+    int estado;
+
+    if(argC > 1) {
+        
+        int n = strtol(argV[1], NULL, 10);
+        pid_t hijo;
+
+        for(int i = 0; i < n; i++) {
+            if(hijo = fork())
+                break;
+            if(hijo == 0){
+                printf("Hijo numero: %d\n",i+1);
+            }
+        }
+
+        if(wait(&estado) == hijo) {
+            printf("soy el papa de %ld, que acaba de terminar\n",(long)getpid());
+        }
+        
+    } else {
+        printf("Debe colocar argumentos\n");
+    }
+
+    return EXIT_SUCCESS;
+}
