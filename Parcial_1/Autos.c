706264229261830c03f80d06b89b9d2931ad5d3d
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2022-04-03 21:49:36 -0500

    [Autos] creado los otros 2 autos y descripción agregada

--- Parcial_1/Autos.c
+++ Parcial_1/Autos.c
@@ -1,6 +1,15 @@
-/* Crea un para  de hilos
-para realizar exclusión mutua +SDL
-*****modo de compilar:  gcc -Wall Autos.c -o Autos  `sdl-config --cflags --libs` -lpthread  */
+/*
+ * 
+ * Crea una ventana y muestra 4 autos en movimiento. Dos de ellos se mueven de izquierda a derecha
+ * y los otros dos se mueven de derecha a izquierda. Se utilizan hilos y exclusión mutua para
+ * mover los autos.
+ *
+ * CCBY: Palestino Hernández Emanuel
+ * Licencia: Apache 2.0
+ *
+ * Modo de Compilar:  gcc -Wall Autos.c -o Autos  `sdl-config --cflags --libs` -lpthread
+ *
+ */
 
 #include <SDL/SDL.h>
 #include <pthread.h>
@@ -16,12 +25,16 @@ SDL_Surface *nave;
 SDL_Rect destino;
 pthread_mutex_t EM;
 
+// Limpia la pantalla
 void pantalla();
+// Ejecutan el movimiento de los autos
 void *funcion1 (void *valor);
 void *funcion2 (void *valor);
+void *funcion3 (void *valor);
+void *funcion4 (void *valor);
 
 int main (int argc, char *argv[]) {
-   pthread_t hilo1,hilo2;
+   pthread_t hilo1, hilo2, hilo3, hilo4;
    pthread_attr_t attr;
 
  /* Inicializar la biblioteca SDL */
@@ -29,8 +42,8 @@ int main (int argc, char *argv[]) {
       printf("No se pudo inicializar SDL: %s\n", SDL_GetError());
       exit(1);
    }
-   fondo=SDL_LoadBMP("fondo.bmp");
-   nave=SDL_LoadBMP("auto.bmp");  
+   fondo = SDL_LoadBMP("fondo.bmp");
+   nave = SDL_LoadBMP("auto.bmp");  
    SDL_SetColorKey(nave, SDL_SRCCOLORKEY,SDL_MapRGB(nave->format, 0, 0, 0));
    screen = SDL_SetVideoMode(1280, 720, 16, SDL_HWSURFACE );
    if( screen == NULL ) {
@@ -47,10 +60,14 @@ int main (int argc, char *argv[]) {
    // Crear hilos
    pthread_create(&hilo1, &attr, funcion1, NULL);
    pthread_create(&hilo2, &attr, funcion2, NULL);
+   pthread_create(&hilo3, &attr, funcion3, NULL);
+   pthread_create(&hilo4, &attr, funcion4, NULL);
 
    //Espera hilos
    pthread_join(hilo1, NULL);
    pthread_join(hilo2, NULL);
+   pthread_join(hilo3, NULL);
+   pthread_join(hilo4, NULL);
 
    SDL_Quit();
    return EXIT_SUCCESS;
@@ -94,6 +111,44 @@ void *funcion2 (void *valor) {
       pantalla();
       destino.x = 0 + j++;
       destino.y = 200 - 160;
+      //if(j==20)j=0;
+      SDL_BlitSurface(nave, NULL, screen, &destino);
+      SDL_Flip(screen);
+      // Fin de Sección Crítica
+      // Apaga Semáforo (FIN Exclusión) para indicar que sale de SC
+      pthread_mutex_unlock(&EM);
+   }
+   pthread_exit(0);
+}
+
+void *funcion3 (void *valor) {
+   int j=0;
+   while(1) {
+      // Enciende Semáforo (INICIO Exclusión) para indicar que entrara a SC
+      pthread_mutex_lock(&EM);
+      // Inicio de Sección Crítica
+      pantalla();
+      destino.x = 800 - j++;
+      destino.y = 400 - 160;
+      //if(j==20)j=0;
+      SDL_BlitSurface(nave, NULL, screen, &destino);
+      SDL_Flip(screen);
+      // Fin de Sección Crítica
+      // Apaga Semáforo (FIN Exclusión) para indicar que sale de SC
+      pthread_mutex_unlock(&EM);
+   }
+   pthread_exit(0);
+}
+
+void *funcion4 (void *valor) {
+   int j=0;
+   while(1) {
+      // Enciende Semáforo (INICIO Exclusión) para indicar que entrara a SC
+      pthread_mutex_lock(&EM);
+      // Inicio de Sección Crítica
+      pantalla();
+      destino.x = 800 - j++;
+      destino.y = 600 - 160;
       //if(j==20)j=0;
       SDL_BlitSurface(nave, NULL, screen, &destino);
       SDL_Flip(screen);
