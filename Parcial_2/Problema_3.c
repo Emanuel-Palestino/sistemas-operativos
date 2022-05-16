bb4aa81280eda3f279df3ee7e76a19aea6bf4686
Emanuel <emanuel.palestino.h@gmail.com>
2022-05-15 23:48:38 -0500

    Archivos innecesarios eliminados

--- Parcial_2/Problema_3.c
+++
@@ -1,9 +0,0 @@
-// Problema 3
-// Uso de hilos y de semaforos para la realización del problema
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <semaphore.h>
-
-float capital = 20000;
